26f3533d4f537a502cd6a560d529244d30b19e41
SammedAdmuthe <sammedadmuthe@gmail.com>
2023-05-29 13:02:33 +0530

    fix success paage navigation

--- registration.js
+++ registration.js
@@ -1,7 +1,9 @@
 var sql = require('mysql');
 var http = require('http');
-var fs  = require('fs').promises;
+var fs  = require('fs');
 var qs = require('qs');
+const { Console } = require('console');
+const { url } = require('inspector');
 
 
 function creatConnection() {
@@ -9,63 +11,109 @@ function creatConnection() {
     var connection = sql.createConnection({
         host: "localhost",
         user: "root",
-        password: "shantivilas@9",
+        password: "root123",
         database: "ecommerce"
     });
     return connection;
 }
+const loginPage = fs.readFileSync('./LoginPage.html')
+const successPage = fs.readFileSync( './success.html')
+// const requestHandler = function(req, res) {
 
-const requestHandler = function(req, res) {
+//     var body = '';
 
-    var body = '';
-    fs.readFile(__dirname + "/LoginPage.html")
-    .then(contents => {
-        res.setHeader("Content-Type", "text/html");
-        // console.log(contents);
-        // console.log(req.form);
+//     fs.readFile(__dirname + "/LoginPage.html")
+//     .then(contents => {
+//         res.setHeader("Content-Type", "text/html");
+//         // console.log(contents);
+//         // console.log(req.form);
+//         // if(req.method === 'POST') {
+//         //     req.on('data', function(chunk){
+//         //         body+=chunk;
+//         //         // console.log(chunk);
+//         //     });
+//         //     req.on('end', function () {
+//         //         var requestedData = JSON.parse(body);
+//         //             console.log(requestedData);
+//         //                     var connection = creatConnection();
+//         //             // var json = qs.parse(body);
+//         //             console.log(requestedData.Username);
+//         //             // console.log(json.pwd);
+
+//         //             // console.log(json.emailAddr);
+
+//         //             connection.connect(function (err) {
+//         //                 var sql = "insert into users values(?,SHA1(?),?)";
+//         //                 connection.query(sql, [requestedData.Username, requestedData.Password, requestedData.EmailAddress], function (err, result) { //query SQL database
+//         //                     if(err) return err;
+//         //                     console.log(result);
+//         //                 });
+//         //                 // window.location.href = "success.html";
+//         //                 connection.end();
+
+//         //                 // res.end(contents);
+
+//         //             });
+//         //             // console.log(json);
+
+//         //     });
+
+//         // }
+//         res.end(contents);
+
+        
+//     })
+
+    
+
+// };
+
+const server = http.createServer((req, res)=>{
+    var url = req.url;
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    if(url == '/') {
+        console.log(req.method);
+        var body = '';
+
         if(req.method === 'POST') {
             req.on('data', function(chunk){
                 body+=chunk;
+                console.log(chunk);
             });
-            // if(req.method === 'POST')
-            // {
-                // console.log(req);
-            //     console.log(req.body);
-    
-    
-            // }
             req.on('end', function () {
-                var connection = creatConnection();
-                var json = qs.parse(body);
-                console.log(json.username);
-                console.log(json.pwd);
+                var requestedData = JSON.parse(body);
+                    console.log(requestedData);
+                            var connection = creatConnection();
+                    // var json = qs.parse(body);
+                    console.log(requestedData.Username);
+                    // console.log(json.pwd);
 
-                console.log(json.emailAddr);
+                    // console.log(json.emailAddr);
 
-                connection.connect(function (err) {
-                    var sql = "insert into users values(?,?,?)";
-                    connection.query(sql, [json.username, json.pwd, json.emailAddr], function (err, result) { //query SQL database
-                        if(err) return err;
-                        console.log(result);
+                    connection.connect(function (err) {
+                        var sql = "insert into users values(?,SHA1(?),?)";
+                        connection.query(sql, [requestedData.Username, requestedData.Password, requestedData.EmailAddress], function (err, result) { //query SQL database
+                            if(err) return err;
+                            console.log(result);
+                        });
+                        // window.location.href = "success.html";
+                        connection.end();
+
+                        // res.end(contents);
+
                     });
-                    connection.end();
+                    // console.log(json);
 
-                    res.end(contents);
-
-                });
-                console.log(json);
-                
-              }); 
+            });
 
         }
+        res.end(loginPage);
+    }
+    else if(url == '/success.html') {
         
-        
-    })
+        res.end(successPage);
+    } 
+});
 
-    
-
-};
-
-const server = http.createServer(requestHandler);
-
-server.listen(8080);
+server.listen(8081);
